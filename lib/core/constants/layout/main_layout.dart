fe3719aee9022a991c1a94334784d4e74e09921c
FORGE096 <forge1803@gmail.com>
2026-06-02 21:57:01 +0330

    feat: added main layout to control the layout of project. cause all screen's header are equal

---
+++ lib/core/constants/layout/main_layout.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MainLayout extends StatelessWidget {
+  final Widget child;
+  const MainLayout({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10, top: 8),
+        child: Column(
+          children: [
+            Container(
+              height: 90,
+              decoration: const BoxDecoration(
+                color: Color(0xFF1E1E2E),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(4),
+                  topRight: Radius.circular(4),
+                  bottomLeft: Radius.circular(18),
+                  bottomRight: Radius.circular(18),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Expanded(child: child),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/presentation/home/home_screen.dart
+++ lib/features/presentation/home/home_screen.dart
@@ -1,30 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:nodejs_shop/core/constants/layout/main_layout.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 8.0),
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 90,
-              decoration: BoxDecoration(
-                color: Color(0xFF1E1E2E), 
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(4.0),
-                  topRight: Radius.circular(4.0),
-                  bottomLeft: Radius.circular(18.0),
-                  bottomRight: Radius.circular(18.0),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return MainLayout(child: Center(child: Text("Home")));
   }
 }
